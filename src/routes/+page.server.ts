b4d52a6f28e11665f01610e21af26cefc21717ce
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-10 17:11:00 +0700

    use nanoid instead

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,9 @@ import { superValidate, message, type Infer } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { fail } from '@sveltejs/kit';
 import { expirationOptions } from '$lib/constants.js';
-import { generateRandomString, alphabet } from 'oslo/crypto';
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 6);
 
 interface Message {
 	url: string;
@@ -22,7 +24,7 @@ export const actions = {
 			return fail(400, { form });
 		}
 
-		const slug = generateRandomString(6, alphabet('a-z', 'A-Z', '0-9'));
+		const slug = nanoid();
 
 		platform?.env.KV.put(slug, form.data.url, {
 			// @ts-expect-error - Keys are guaranteed to be present
